9926257167e33f9f1701127787b24f92dde6f595
mohammadreza jamshidi <138961578+mamali47@users.noreply.github.com>
2026-09-13 15:58:18 +0330

    Update urls.py

--- arzlearn_backend/accounts/urls.py
+++ arzlearn_backend/accounts/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 
-from .views import LoginAPIView, LogoutAPIView, MeAPIView, RegisterAPIView
+from .views import (
+    LoginAPIView,
+    LogoutAPIView,
+    MeAPIView,
+    PasswordResetConfirmAPIView,
+    PasswordResetRequestAPIView,
+    RegisterAPIView,
+    ResendVerificationEmailAPIView,
+    VerifyEmailAPIView,
+)
 
 app_name = 'accounts'
 
@@ -9,4 +18,8 @@ urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('me/', MeAPIView.as_view(), name='me'),
+    path('verify-email/', VerifyEmailAPIView.as_view(), name='verify-email'),
+    path('resend-verification/', ResendVerificationEmailAPIView.as_view(), name='resend-verification'),
+    path('password-reset/', PasswordResetRequestAPIView.as_view(), name='password-reset'),
+    path('password-reset-confirm/', PasswordResetConfirmAPIView.as_view(), name='password-reset-confirm'),
 ]
