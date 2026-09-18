7a721a1af3301723468410a814cf40ac105f04c6
mohammadreza jamshidi <138961578+mamali47@users.noreply.github.com>
2026-09-18 13:05:00 +0330

    Update serializers.py

--- arzlearn_backend/accounts/serializers.py
+++ arzlearn_backend/accounts/serializers.py
@@ -68,13 +68,6 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         return attrs
 
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = CustomUser(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
-
 
 class LoginSerializer(serializers.Serializer):
     """
